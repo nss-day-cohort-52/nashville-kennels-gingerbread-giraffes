459df4be120ea5b164b9f2111cbc9b0df181e814
charleslong13 <charleslong13@icloud.com>
2021-12-01 10:16:52 -0600

    csl-premerge commit

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -84,7 +84,7 @@ export const Animal = ({ animal, syncAnimals,
                         <section>
                             <h6>Caretaker(s)</h6>
                             <span className="small">
-                                Unknown
+                                
                             </span>
 
 
